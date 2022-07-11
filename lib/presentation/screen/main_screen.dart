b1e973207cf0be0d4bcd22b34766889b18bb7361
Aashish-Dahal <ashishdahal490@gmail.com>
2022-07-11 17:34:16 +0545

    fix: Main Screen

--- lib/presentation/screen/main_screen.dart
+++ lib/presentation/screen/main_screen.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/material.dart'
-    show
-        BuildContext,
-        Colors,
-        Key,
-        MaterialApp,
-        StatelessWidget,
-        ThemeData,
-        Widget;
+    show BuildContext, Key, MaterialApp, StatelessWidget, Widget;
 import 'package:jandj_customer/config/app_theme/app_theme.dart';
 import 'package:jandj_customer/presentation/screen/home_screen.dart';
 
