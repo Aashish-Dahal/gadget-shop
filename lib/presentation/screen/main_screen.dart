8107ff448750fcf4891f28ef28537c9c51ee47b8
Aashish-Dahal <ashishdahal490@gmail.com>
2022-07-12 17:48:22 +0545

    fix: Main Screen

--- lib/presentation/screen/main_screen.dart
+++ lib/presentation/screen/main_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart'
     show BuildContext, Key, MaterialApp, StatelessWidget, Widget;
+import 'package:jandj_customer/business/provider/auth_provider.dart';
+import 'package:provider/provider.dart'
+    show ChangeNotifierProvider, MultiProvider, ReadContext;
 import 'package:jandj_customer/config/app_theme/app_theme.dart';
-import 'package:jandj_customer/presentation/screen/home_screen.dart';
+import 'package:jandj_customer/presentation/screen/auth_screen.dart';
+
+import '../../config/app_route/app_route.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -9,10 +14,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'J&J Suppliers',
-      theme: AppTheme.light,
-      home: const HomeScreen(),
-    );
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (_) => AuthProvider(),
+          )
+        ],
+        builder: (context, child) {
+          return MaterialApp(
+            title: 'J&J Suppliers',
+            theme: AppTheme.light,
+            onGenerateRoute: (settings) {
+              return AppRouter.onGenerateRoutes(
+                  settings, context.read<AuthProvider>().loggedIn);
+            },
+            initialRoute: AuthScreen.routeName,
+          );
+        });
   }
 }
